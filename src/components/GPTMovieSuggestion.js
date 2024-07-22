327e77a3404b0dc1cd7ec3c6a443f5ded3a73ffe
Ambar Ahmad <94756332+Ambar2003@users.noreply.github.com>
2024-07-22 23:09:30 +0530

    Update GPTMovieSuggestion.js

--- src/components/GPTMovieSuggestion.js
+++ src/components/GPTMovieSuggestion.js
@@ -3,15 +3,15 @@ import { useSelector } from 'react-redux'
 import MovieList from './MovieList';
 
 const GPTMovieSuggestion = () => {
-  const{movieResults,movieName} = useSelector((store) => store.gpt);
-  if(!movieName) return null;
+  const{movieResults,movieNames} = useSelector((store) => store.gpt);
+  if(!movieNames || !movieResults || !movieNames.length || !movieResults.length) return null;
   return (
     <div className="p-4 m-4 bg-black text-white bg-opacity-70">
       <div>
         {movieName.map((movieName,index)=>(
         <MovieList
         key = {movieName} 
-        title ={movieName} 
+        title = {movieName} 
         movies={movieResults[index]}
         />
         ))}
